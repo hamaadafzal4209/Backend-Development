848c9b3fb37c3d6db10b25436f52e129e3099458
FrontendFlair <hamaadafzal404@gmail.com>
2024-05-21 08:57:19 -0700

    make login, sign up and logout functionallity with ejs using bcrypt and jsonwebtoken

---
+++ Authentication With Ejs/model/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/authentication')
+
+const userSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    age: Number
+})
+
+module.exports = mongoose.model("user", userSchema)
