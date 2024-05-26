17b7f2c2d56cdbbb7034e987b75afdd54351326f
FrontendFlair <hamaadafzal404@gmail.com>
2024-05-26 03:13:58 -0700

    Add Custom Styling

--- MUI Demo/src/App.jsx
+++ MUI Demo/src/App.jsx
@@ -1,27 +1,63 @@
-import { Typography, Container, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, AppBar, Toolbar, Button } from '@mui/material';
-import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import {
+  Typography,
+  Container,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  AppBar,
+  Toolbar,
+} from "@mui/material";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
 
 function App() {
   return (
     <>
       <CssBaseline />
-      <AppBar position='relative'>
+      <AppBar position="relative">
         <Toolbar>
-          <CameraAltIcon />
-          <Typography variant='h6'>
-            Photo Album
-          </Typography>
+          <CameraAltIcon sx={{ mr: 0.5 }} />
+          <Typography variant="h6">Photo Album</Typography>
         </Toolbar>
       </AppBar>
       <main>
         <div>
-          <Container maxWidth='sm'>
-            <Typography variant='h2' align='center' color='textPrimary' gutterBottom >
+          <Container maxWidth="sm">
+            <Typography
+              variant="h3"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
               Photo Album
             </Typography>
-            <Typography variant='h5' align='center' color='textSecondary' paragraph >
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, provident incidunt rem magni in velit laudantium nostrum adipisci dolores perspiciatis? 
+            <Typography
+              variant="h6"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Aspernatur, provident incidunt rem magni in velit laudantium
+              nostrum adipisci dolores perspiciatis?
             </Typography>
+            <div>
+              <Grid container spacing={2} justifyContent="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See My Photo
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    See My Album
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
           </Container>
         </div>
       </main>
